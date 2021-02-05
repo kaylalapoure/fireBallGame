0350adff908cf1be3b71b0f8da98fffb6e006929
kaylalapoure <78117204+kaylalapoure@users.noreply.github.com>
2021-02-05 17:01:04 -0600

    vr 2.0
    yes, vrrrrr

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Transform effect;
+
+    private Rigidbody rb;
+    private int count;
+    
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText ();
+        winText.text = "";
+
+    }
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+        
+        rb.AddForce (movement * speed);
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            Instantiate(effect, other.transform.position, Quaternion.identity);
+            count = count + 1;
+            SetCountText ();
+        }
+    }
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString ();
+        if(count >= 17)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
